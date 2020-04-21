d36a10a8e6502dde54ae92d1c34ed5c78d55acc3
Lu7fer <59082785+Lu7fer@users.noreply.github.com>
2020-04-21 10:36:21 +0800

    week08

---
+++ times15_q01.c
@@ -0,0 +1,19 @@
+/** 
+ * @Author : Lu7fer
+ * @Date: 2020-04-21 10:26:12
+ * @LastEditTime: 2020-04-21 10:26:13
+ * @FilePath: \C-language-study-homework\times15_q01.c
+ * @Stu_ID: 2019X....X229_Lu7fer
+ * @Github: https://github.com/Lu7fer/C-language-study-homework
+ * @Copyright
+-------------------------------------------
+Copyright (C) 2020 - Lu7fer
+C-language-study-homework is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+ 
+You should have received a copy of the GNU General Public License along with C-language-study-homework. 
+If not, see <http: //www.gnu.org/licenses/>.
+-------------------------------------------
+ */
+
