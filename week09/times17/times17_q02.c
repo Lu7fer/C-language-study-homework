6082528c7234b6786e518c5f110087b1fb76aa6a
Lu7fer <59082785+Lu7fer@users.noreply.github.com>
2020-04-29 11:36:04 +0800

    Revert "week09"
    This reverts commit b1cb90f0f00674af521fe5b43117a81951e8364c.

---
+++ week09/times17/times17_q02.c
@@ -0,0 +1,20 @@
+/** 
+ * @Author : Lu7fer
+ * @Date: 2020-04-28 17:10:07
+ * @LastEditTime: 2020-04-28 17:10:08
+ * @FilePath: \C-language-study-homework\week09\times17\times17_q02.c
+ * @Stu_ID: 2019X....X229_Lu7fer
+ * @Github: https://github.com/Lu7fer/C-language-study-homework
+ * @Copyright
+-------------------------------------------
+Copyright (C) 2020 - Lu7fer
+C-language-study-homework is free software:
+    you can redistribute it and/or modify it under the terms of 
+the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+ 
+You should have received a copy of the GNU General Public License 
+along with C-language-study-homework. 
+If not, see <http: //www.gnu.org/licenses/>.
+-------------------------------------------
+ */
