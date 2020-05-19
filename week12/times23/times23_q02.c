344dc86052c786148b562b28eaf56d617697768e
Lu7fer <59082785+Lu7fer@users.noreply.github.com>
2020-05-19 12:32:58 +0800

    times 23rd

--- week12/times23/times23_q02.c
+++ week12/times23/times23_q02.c
@@ -13,7 +13,7 @@ C-language-study-homework is free software:
 the GNU General Public License as published by the Free Software Foundation,
 either version 3 of the License, or (at your option) any later version.
  
-You should have received a copy of the GNU General Public License 
+ You should have received a copy of the GNU General Public License 
 along with C-language-study-homework. 
 If not, see <http: //www.gnu.org/licenses/>.
 -------------------------------------------
