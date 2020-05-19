69ca9143d127c5662a7645a8455d8012817b3f83
Lu7fer <59082785+Lu7fer@users.noreply.github.com>
2020-05-19 11:05:10 +0800

    Update times22_q01.c

--- week11/times22/times22_q01.c
+++ week11/times22/times22_q01.c
@@ -5,18 +5,6 @@
  * @FilePath: \C-language-study-homework\week11\times22\times22_q01.c
  * @Stu_ID: 2019X....X229_Lu7fer
  * @Github: https://github.com/Lu7fer/C-language-study-homework
- * @Copyright
--------------------------------------------
-Copyright (C) 2020 - Lu7fer
-C-language-study-homework is free software:
-    you can redistribute it and/or modify it under the terms of 
-the GNU General Public License as published by the Free Software Foundation,
-either version 3 of the License, or (at your option) any later version.
- 
-You should have received a copy of the GNU General Public License 
-along with C-language-study-homework. 
-If not, see <http: //www.gnu.org/licenses/>.
--------------------------------------------
  */
 
 #include <stdio.h>
